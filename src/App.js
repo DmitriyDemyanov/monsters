44b775eae1f60dc510b8461f34b59900ca1b485e
Dmitriy <dimaprostokosmos@gmail.com>
2023-05-29 16:58:55 +0300

    promise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    }
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        data.push({ name: 'Hena',id: 12765 });
+        return data;
+      })
+      .then(users => this.setState(
+        () => ({ monsters: users }),
+        () => console.log(this.state)
+      ))
+      .catch((err) => console.log('ERROR!!!!',err.message || err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.monsters.map((el) => {
+            return (
+              <div key={el.id}>
+                <h1> {el.name} </h1>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+//
